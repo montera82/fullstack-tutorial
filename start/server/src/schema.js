c9c92ef378f421c9b1a53ad188cf37bc6a650501
ikechukwu edwin <eo@hellofresh.com>
2018-11-10 13:03:02 +0100

    Setting up schema.

---
+++ start/server/src/schema.js
@@ -0,0 +1,63 @@
+const {gql} = require('apollo-server');
+
+const typeDefs = gql`
+
+type Query {
+    launches: [Launch]!
+    # Queries for the current user
+    launch(id: ID!): Launch
+    me: User
+}
+
+type Mutation {
+    # if false, signup failed -- check errors
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+
+    # if false, cancellation failed -- check errors
+    cancelTrip(launchId: ID!): TripUpdateResponse!
+
+    login(email: String): String #login token
+
+}
+
+
+
+type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+}
+
+type Rocket {
+    id: ID!
+    name: String
+    type: String
+}
+
+type User {
+    id: ID
+    email: String
+    trips: [Launch]!
+}
+
+type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+}
+
+enum PatchSize {
+    SMALL
+    LARGE
+}
+
+type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+}
+`
+
+module.exports = typeDefs;
+
